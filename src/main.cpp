78f37b712c3d47e661896b665cd34d1e0f0501d8
Eduardo Lemos Rocha <dudulr10@gmail.com>
2018-06-29 22:21:22 -0300

    Ordem adicionada

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,26 @@
 #include "ArvoreB.hpp"
 #include "In_OutPuts.hpp"
 
-int main(){
-    ArvoreB teste(3);
-    int size = HowManyLines("lista1.txt");
-    cout<<size<<endl;
-    char** Chaves = BuildPrimaryKey("lista1", size);
-    // for(int i = 0; i < size; i++){
-    //     cout<<Chaves[i]<<endl;
-    // }
-    for(int i = 0; i < size; i++){
-        teste.Insere_ArvoreB(Chaves[i]);
-        cout<<Chaves[i]<<endl;
+int main(int argc, char*argv[]){
+    int ordem = atoi(argv[1]);
+    if(ordem %2 == 0){
+        ArvoreB teste((ordem / 2));
+        int size = HowManyLines("lista1.txt");
+        char** Chaves = BuildPrimaryKey("lista1", size);
+        for(int i = 0; i < size; i++){
+            teste.Insere_ArvoreB(Chaves[i]);
+            cout<<Chaves[i]<<endl;
+        }
+    }else{
+        ArvoreB teste((ordem / 2) + 1);
+        int size = HowManyLines("lista1.txt");
+        char** Chaves = BuildPrimaryKey("lista1", size);
+        for(int i = 0; i < size; i++){
+            teste.Insere_ArvoreB(Chaves[i]);
+            cout<<Chaves[i]<<endl;
+        }
     }
+    
 
     // cout<<"Inicialmente\n\n\n"<<endl; //é o print do C++
     // teste.Percorre_ArvoreB();
